9c95fae50799c9f0a5a5645b5f7a4d95ae5a7265
shawn <1524587436@qq.com>
2025-09-29 16:02:52 +0800

    添加并发编程相关知识

--- src/main/java/com/example/threadlearning/ThreadLearningApplication.java
+++ src/main/java/com/example/threadlearning/ThreadLearningApplication.java
@@ -14,12 +14,6 @@ public class ThreadLearningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThreadLearningApplication.class, args);
-        System.out.println("线程池学习应用启动成功！");
-        System.out.println("测试接口：");
-        System.out.println("- 测试默认异步任务: http://localhost:8080/thread-pool/test-default");
-        System.out.println("- 测试自定义线程池: http://localhost:8080/thread-pool/test-custom");
-        System.out.println("- 测试IO线程池: http://localhost:8080/thread-pool/test-io");
-        System.out.println("- 测试多个任务: http://localhost:8080/thread-pool/test-multiple");
     }
 
 }
